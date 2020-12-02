d5f66f2676ace34714cfffeef7170563f3ba7a3a
Cezar <cezar.sousa@sysmap.com.br>
2020-12-02 19:30:37 -0300

    Refactor structure

---
+++ TestsLeetCode/singleNumberUT.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestsLeetCode
+{
+    [TestClass]
+    public class singleNumberUT
+    {
+        [TestMethod]
+        public void doIt()
+        {
+        }
+    }
+}
