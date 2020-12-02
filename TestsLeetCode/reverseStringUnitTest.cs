a664c0300d9c24d952b641b03df13b23591760ea
Cezar <cezar.sousa@sysmap.com.br>
2020-12-02 17:16:22 -0300

    gitignore in tests

---
+++ TestsLeetCode/reverseStringUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestsLeetCode
+{
+    [TestClass]
+    public class reverseStringUnitTest
+    {
+        [TestMethod]
+        public void testReverseString()
+        {               
+            verifyReverseString("cezar", "razec");
+            verifyReverseString("banana", "ananab");
+            verifyReverseString("123456789123", "123987654321");
+            verifyReverseString("","");
+        }
+        private void verifyReverseString(string input,string vs_expected)
+        {
+            char[] output = reverseString.reverseStringMethod(input.ToCharArray());
+            char[] expected = vs_expected.ToCharArray();
+            output.Should().BeEquivalentTo(expected);
+        }        
+    }
+}
