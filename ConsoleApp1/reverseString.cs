3733301c90b28ed64f5c660e4b9feb3307369cb2
Cezar <cezar.sousa@sysmap.com.br>
2020-12-02 16:48:19 -0300

    reverse string

---
+++ ConsoleApp1/reverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public  static class reverseString
+{
+    public static char[] reverseStringMethod(char[] s)
+    {
+        int i = 0 ;
+        char aux;
+        if (s.Length > 0)
+        {
+            do
+            {
+                aux = s[i];
+                s[i] = s[(s.Length - 1) - i];
+                s[(s.Length - 1) - i] = aux;
+                i++;
+            } while (i < Math.Floor(Convert.ToDecimal(s.Length / 2)));
+        }
+        return s; 
+    }
+}
+
