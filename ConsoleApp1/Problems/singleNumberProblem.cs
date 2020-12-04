3ade8c0e507e914bc3504ff722407ce5ccc67f09
Cezar <cezar.sousa@sysmap.com.br>
2020-12-03 23:21:20 -0300

    solution using XOR

--- ConsoleApp1/Problems/singleNumberProblem.cs
+++ ConsoleApp1/Problems/singleNumberProblem.cs
@@ -1,43 +1,12 @@
 ﻿public static class singleNumberProblem
 {
     public static int implementation(int[] nums){
-                
-        int i = 0;
-        int j = nums.Length - 1;
-        int aux = 0;
-        int cont = 0; 
-        int choosenOne = 0;
-        
-        if (nums.Length == 1)
-            return nums[0];
+        int ans = 0;
+        int len = nums.Length;
+        for (int i = 0; i != len; i++)
+            ans ^= nums[i];
 
-        do
-        {
-            do
-            {
-                if (nums[i] == nums[j])
-                {
-                    aux = nums[i + 1];
-                    nums[i + 1] = nums[j];
-                    nums[j] = aux;
-                    cont++;
-                }
-                j--;
-            } while (j > 0);
-                        
-            if (cont == 0)
-            {
-                choosenOne = nums[i];
-                i = nums.Length;
-            }
-            else
-            {
-                i += 2;
-                j = nums.Length - 1;
-                choosenOne = nums[i];
-            }           
-        } while (i < (nums.Length -1));
-        return choosenOne;         
+        return ans;
     }
 }
 
