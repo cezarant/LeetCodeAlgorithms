69f08d73f92148a6b1e35dfe840778900e1d96a5
Cezar <cezar.sousa@sysmap.com.br>
2020-12-09 10:42:08 -0300

    DeleteNodeLinkedList

---
+++ ConsoleApp1/Problems/DeleteNodeLinkedListProblem.cs
@@ -0,0 +1,15 @@
+﻿public static class DeleteNodeLinkedListProblem
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    public static void implementation(ListNode node)
+    {        
+         node.val = node.next.val;
+         node.next = node.next.next;        
+    }
+}
+
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace ConsoleApp1
+﻿using System;
+
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            singleNumberProblem.implementation(new int[] { 2, 2, 1 });
+            singleNumberProblem.implementation(new int[] { 4, 1, 2, 1, 2 });
+            Console.ReadKey();
         }
     }
 }
