ee9b051f1c7d3ad8d95ba82daa19ef17ade2d976
Cezar <cezar.sousa@sysmap.com.br>
2020-12-11 21:16:31 -0300

    majority element

---
+++ ConsoleApp1/Problems/majorityElementProblem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class majorityElementProblem
+{
+    public static int implementation(int[] nums)
+    {
+        Array.Sort(nums);         
+        int n = nums.Length / 2;         
+        return nums[n];
+    }
+}
+
