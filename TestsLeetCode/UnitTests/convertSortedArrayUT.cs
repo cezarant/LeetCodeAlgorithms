5b3dffe09d21cb3cd333327550e4491e80601882
cezar <cezarantoniosouza@hotmail.com>
2020-12-17 11:02:42 -0300

    Convert Sorted Array to BST

---
+++ TestsLeetCode/UnitTests/convertSortedArrayUT.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsLeetCode.UnitTests
+{
+    [TestClass]
+    public class convertSortedArrayUT
+    {
+        [TestMethod]
+        public void doIt()
+        {
+            // TreeNode treeNode = convertSortedArrayBST.implementation(new int[] { -10, -3, 0, 5, 9 });
+            // treeNode.Should().Be(null);
+        }
+    }
+}
