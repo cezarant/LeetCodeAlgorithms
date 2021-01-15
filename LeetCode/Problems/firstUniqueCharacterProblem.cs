e14e9d3a6a0c0493ed907b09cc17b7799f48b81d
Cezar Antonio de Sousa <cezar.sousa@sysmap.com.br>
2021-01-15 19:32:05 -0300

    First Unique Character Implementation

---
+++ LeetCode/Problems/firstUniqueCharacterProblem.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace ConsoleApp1.Problems
+{
+    public static class firstUniqueCharacterProblem
+    {
+        public static int implementation(string s){
+            var charAndCount = new int[256];
+
+            foreach (var c in s)
+            {
+                charAndCount[c]++;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charAndCount[s[i]] == 1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ TestLeetCodeAlgorithms/UnitTests/firstUniqueCharacterUT.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using ConsoleApp1.Problems;
+
+namespace TestLeetCodeAlgorithms.UnitTests
+{
+    
+    [TestClass]
+    public class firstUniqueCharacterUT
+    {
+        [TestMethod]
+        public void doIt()
+        {
+            int result = firstUniqueCharacterProblem.implementation("leetcode");
+            result.Should().Be(0);
+
+            result = firstUniqueCharacterProblem.implementation("loveleetcode");
+            result.Should().Be(2);
+        }
+    }
+}
