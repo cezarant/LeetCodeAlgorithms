814ee1e8d45d064e35a65132829cce68399b47d6
Cezar Antonio de Sousa <cezar.sousa@sysmap.com.br>
2021-01-19 17:28:39 -0300

    Climbimg Stairs Implementation

---
+++ LeetCode/Problems/climbingStairsProblem.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1.Problems
+{
+    public static class climbingStairsProblem
+    {
+        public static int implementation(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            int first = 1;
+            int second = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                int third = first + second;
+                first = second;
+                second = third;
+            }
+            return second;
+        }
+    }
+}
---
+++ TestLeetCodeAlgorithms/UnitTests/climbimgStairsUT.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1.Problems;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class climbimgStairsUT
+{
+    [TestMethod]
+    public void doIt()
+    {
+        int n = climbingStairsProblem.implementation(2);
+        n.Should().Be(2);
+
+        n = climbingStairsProblem.implementation(3);
+        n.Should().Be(3);
+    }
+}
+
