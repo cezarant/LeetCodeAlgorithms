0dd90c8e5fd7ceae0f2d8c85cd70b9b6d9b652f4
Cezar Antonio de Sousa <cezar.sousa@sysmap.com.br>
2021-01-20 18:12:09 -0300

    ValidParentesisUT and outhers

---
+++ LeetCode/Problems/validParentesisProblem.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1.Problems
+{
+    public static class validParentesisProblem
+    {
+        public static bool implementation(string s){
+            if (s == "") return true;
+
+            var stack = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (c == ')' && (!stack.Any() || stack.Pop() != '('))
+                    {
+                        return false;
+                    }
+                    else if (c == ']' && (!stack.Any() || stack.Pop() != '['))
+                    {
+                        return false;
+                    }
+                    else if (c == '}' && (!stack.Any() || stack.Pop() != '{'))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return !stack.Any();
+        }
+    }
+}
