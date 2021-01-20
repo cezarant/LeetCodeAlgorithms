9bdac6c663cc7ddf2d6c78fafbdcb6b875261821
Cezar Antonio de Sousa <cezar.sousa@sysmap.com.br>
2021-01-20 19:26:59 -0300

    Frist try

---
+++ TestLeetCodeAlgorithms/UnitTests/isPalindromeUT.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp1.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace TestLeetCodeAlgorithms.UnitTests
+{
+    
+    [TestClass]
+    public class isPalindromeUT
+    {
+        [TestMethod]
+        public void doIt()
+        {
+            bool result = IsPalindromeProblem.implementation("A man, a plan, a canal: Panama");
+            result.Should().Be(true);
+
+            result = IsPalindromeProblem.implementation("race a car");
+            result.Should().Be(false);
+
+
+        }
+    }
+}
---
+++ TestLeetCodeAlgorithms/UnitTests/validParentesisUT.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace TestLeetCodeAlgorithms.UnitTests
+{    
+    [TestClass]
+    public class validParentesisUT
+    {
+        [TestMethod]
+        public void doIt()
+        {
+            validParentesisProblem.implementation("()").Should().BeTrue();
+            validParentesisProblem.implementation("()[]{}").Should().BeTrue();
+            validParentesisProblem.implementation("(]").Should().BeFalse();
+            validParentesisProblem.implementation("([)]").Should().BeFalse();
+            validParentesisProblem.implementation("{[]}").Should().BeTrue();
+        }
+    }
+}
