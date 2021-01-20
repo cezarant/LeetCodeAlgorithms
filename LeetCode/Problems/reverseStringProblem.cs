ba2103c1db8516f2a5d5ace16a25c6b56b94152a
Cezar Antonio de Sousa <cezar.sousa@sysmap.com.br>
2021-01-20 19:25:10 -0300

    First try

--- LeetCode/Problems/reverseStringProblem.cs
+++ LeetCode/Problems/reverseStringProblem.cs
@@ -2,21 +2,24 @@
 
 public static class reverseStringProblem
 {
+    // Write a function that reverses a string. The input string is given as an array of characters char[].
+    // Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+    // You may assume all the characters consist of printable ascii characters.
     public static char[] implementation(char[] s)
     {
-        int i = 0;
-        char aux;
-        if (s.Length > 0)
+        // Example: cezar -> razec
+        // cezar -> aux:c 
+        // cezar -> c:r 
+        // rezar -> r:c 
+        // rezac.... 
+        char  aux = ' ';
+        for (int i = 0; i < s.Length; i++)
         {
-            do
-            {
-                aux = s[i];
-                s[i] = s[(s.Length - 1) - i];
-                s[(s.Length - 1) - i] = aux;
-                i++;
-            } while (i < Math.Floor(Convert.ToDecimal(s.Length / 2)));
+            aux = s[s.Length - 1];
+            s[i] = aux;
+            s[s.Length - 1] = s[i];
         }
-        return s;
+        return s; 
     }
 }
 
