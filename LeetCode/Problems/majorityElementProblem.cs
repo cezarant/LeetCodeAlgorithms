61dc061f554e1faa34abad677a0ce64db39a257f
Cezar Antonio de Sousa <cezar.sousa@sysmap.com.br>
2021-01-25 08:06:39 -0300

    Update

---
+++ LeetCode/Problems/majorityElementProblem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Problems
+{
+    public static class majorityElementProblem
+    {
+        public static int implementation(int[] r)
+        {
+            throw new Exception("");
+        }
+    }
+}
