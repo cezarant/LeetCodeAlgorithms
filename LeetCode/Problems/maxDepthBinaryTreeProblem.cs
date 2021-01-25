2f508b42e210fa8499cc30e7abcf703b1365b616
Cezar Antonio de Sousa <cezar.sousa@sysmap.com.br>
2021-01-25 19:58:00 -0300

    MaxDepthBinaryTree

--- LeetCode/Problems/maxDepthBinaryTreeProblem.cs
+++ LeetCode/Problems/maxDepthBinaryTreeProblem.cs
@@ -1,18 +1,14 @@
-
 using System;
-
 public class TreeNode
 {
     public int? val;
     public TreeNode left;
     public TreeNode right;
     private object p;
-
     public TreeNode(object p)
     {
         this.p = p;
     }
-
     public TreeNode(int? val = 0, TreeNode left = null, TreeNode right = null)
     {
         this.val = val;
@@ -22,9 +18,18 @@ public class TreeNode
 }
 public static class maxDepthBinaryTreeProblem
 {
+    // Given the root of a binary tree, return its maximum depth.
+    // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.   
     public static int implementation(TreeNode root)
     {
-        throw new Exception("Not implemented");
+        if (root == null)
+            return 0;
+        
+        int leftDef = implementation(root.left); 
+        int rightDef = implementation(root.right);
+        if (leftDef > rightDef) 
+            return (leftDef + 1); 
+        else 
+            return (rightDef + 1);  
     }
 }
-
