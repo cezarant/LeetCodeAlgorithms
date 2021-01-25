6f856a7f0740a8fe759a7f598b98475202615af7
Cezar Antonio de Sousa <cezar.sousa@sysmap.com.br>
2021-01-25 08:55:09 -0300

    valid anagram implementation

--- LeetCode/Problems/validAnagramProblem.cs
+++ LeetCode/Problems/validAnagramProblem.cs
@@ -1,11 +1,16 @@
-
 using System;
+using System.Linq;
 
 public static class validAnagramProblem
 {
     public static bool implementation(string s, string t)
     {
-        throw new Exception("Not implemented");
+        char[] arr1 = s.ToCharArray();
+        char[] arr2 = t.ToCharArray();
+       
+        Array.Sort(arr1);
+        Array.Sort(arr2);
+        return arr1.SequenceEqual(arr2);
     }
 }
 
