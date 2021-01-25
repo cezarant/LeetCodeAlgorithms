2f4ded961109222cb59b212c32b4a49810827be0
Cezar Antonio de Sousa <cezar.sousa@sysmap.com.br>
2021-01-25 20:12:14 -0300

    Contains Duplicate problem

--- LeetCode/Problems/containsDuplicateProblem.cs
+++ LeetCode/Problems/containsDuplicateProblem.cs
@@ -2,6 +2,9 @@
 
 namespace ConsoleApp1.Problems
 {
+    // Given an array of integers, find if the array contains any duplicates.
+    // Your function should return true if any value appears at least twice in the array, 
+    // and it should return false if every element is distinct.
     public static class containsDuplicateProblem
     {
         public static bool implementation(int[] nums){
