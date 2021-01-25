de23cffcbc043a7541ef53d90fb5be8184b67956
Cezar Antonio de Sousa <cezar.sousa@sysmap.com.br>
2021-01-25 14:17:38 -0300

    Ispalindrome

--- LeetCode/Problems/IsPalindromeProblem.cs
+++ LeetCode/Problems/IsPalindromeProblem.cs
@@ -1,14 +1,24 @@
-
-
 using System;
 
 namespace ConsoleApp1.Problems
 {
+    // Given a string, determine if it is a palindrome, 
+    // considering only alphanumeric characters and ignoring cases.
+    // Note: For the purpose of this problem, we define empty string as valid palindrome.
     public static class IsPalindromeProblem
     {
         public static bool implementation(string s)
         {
-            throw new Exception("Not implemented");
+            char[] arr = s.Replace(" ",string.Empty).Trim().ToLower().ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (char.IsLetterOrDigit(arr[i]))
+                {
+                    if (arr[i] != arr[(arr.Length - 1) - i])
+                        return false;
+                }
+            }            
+            return true; 
         }
     }
 }
---
+++ TestLeetCodeAlgorithms/UnitTests/validPalindromeUT.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace TestLeetCodeAlgorithms.UnitTests
+{    
+    [TestClass]
+    public class validPalindromeUT
+    {        
+        [TestMethod]
+        public void doIt()
+        {
+            IsPalindromeProblem.implementation("A man, a plan, a canal: Panama").Should().Be(true);
+            IsPalindromeProblem.implementation("race a car").Should().Be(false);
+        }
+    }
+}
