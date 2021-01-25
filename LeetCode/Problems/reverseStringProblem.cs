98029f8fbf6f24a14f938664bf8f0834eb6e774f
Cezar Antonio de Sousa <cezar.sousa@sysmap.com.br>
2021-01-25 08:49:36 -0300

    reverseString

--- LeetCode/Problems/reverseStringProblem.cs
+++ LeetCode/Problems/reverseStringProblem.cs
@@ -5,7 +5,17 @@ public static class reverseStringProblem
 
     public static char[] implementation(char[] s)
     {
-        throw new Exception("Not implemented");
+        if (s.Length == 0) return s; 
+        int cont = 0;
+        char aux = ' '; 
+        do
+        {
+            aux = s[s.Length - (cont + 1)];
+            s[s.Length - (cont+ 1)] = s[cont];
+            s[cont] = aux;
+            cont++;
+        } while ((cont + 1 ) < s.Length);
+        return s;
     }
 }
 
