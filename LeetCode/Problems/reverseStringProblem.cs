cc433e04032efaf0353087d2edc523ad8923cee1
Cezar Antonio de Sousa <cezar.sousa@sysmap.com.br>
2021-01-26 10:23:45 -0300

    Large text files to test

--- LeetCode/Problems/reverseStringProblem.cs
+++ LeetCode/Problems/reverseStringProblem.cs
@@ -1,19 +1,34 @@
+using System;
+
 public static class reverseStringProblem
 {
     // Write a function that reverses a string.The input string is given as an array of characters char[].
     // Do not allocate extra space for another array,
     // you must do this by modifying the input array in-place with O(1) extra memory.
-    public static char[] implementation(char[] listaCaracteres)
+    public static char[] implementation(char[] s)
     {
-        if (listaCaracteres.Length == 0) return listaCaracteres;
-        char[] revertedArr = new char[listaCaracteres.Length];
+        /*if (s.Length == 0) return s;
+        char[] revertedArr = new char[s.Length];
         int cont = 0;
-        for (int i = listaCaracteres.Length - 1; i >= 0 ; i--)
+        for (int i = s.Length - 1; i >= 0; i--)
         {
-            revertedArr[cont] = listaCaracteres[i];
-            cont++; 
+            revertedArr[cont] = s[i];
+            cont++;
         }
-        return revertedArr;       
+        return revertedArr;*/
+        int i = 0;
+        char aux;
+        if (s.Length > 0)
+        {
+            do
+            {
+                aux = s[i];
+                s[i] = s[(s.Length - 1) - i];
+                s[(s.Length - 1) - i] = aux;
+                i++;
+            } while (i < Math.Floor(Convert.ToDecimal(s.Length / 2)));
+        }
+        return s; 
     }
 }
 
--- TestLeetCodeAlgorithms/UnitTests/reverseStringUT.cs
+++ TestLeetCodeAlgorithms/UnitTests/reverseStringUT.cs
@@ -8,10 +8,15 @@ public class reverseStringUnitTest
     [TestMethod]
     public void doIt()
     {
-        reverseStringProblem.implementation("cezar".ToCharArray()).Should().BeEquivalentTo("razec");
-        reverseStringProblem.implementation("banana".ToCharArray()).Should().BeEquivalentTo("ananab");
-        reverseStringProblem.implementation("123456789123".ToCharArray()).Should().BeEquivalentTo("123987654321");
-        reverseStringProblem.implementation("".ToCharArray()).Should().BeEquivalentTo(string.Empty);
+        reverseStringProblem.implementation("cezar".ToCharArray()).Should().Equal("razec".ToCharArray());
+        reverseStringProblem.implementation("banana".ToCharArray()).Should().Equal("ananab".ToCharArray());
+        reverseStringProblem.implementation("123456789123".ToCharArray()).Should().Equal("321987654321".ToCharArray());
+
+        string contentFile = System.IO.File.ReadAllText(string.Format(@"{0}\{1}", System.IO.Directory.GetParent(".."), @"TestCases\1.txt"));
+        string contentFileReverted = System.IO.File.ReadAllText(string.Format(@"{0}\{1}", System.IO.Directory.GetParent(".."), @"TestCases\1reverted.txt"));
+        reverseStringProblem.implementation(contentFile.ToCharArray()).Should().Equal(contentFileReverted.ToCharArray());
+        
+        reverseStringProblem.implementation("".ToCharArray()).Should().Equal(string.Empty);
     }
 }
 
