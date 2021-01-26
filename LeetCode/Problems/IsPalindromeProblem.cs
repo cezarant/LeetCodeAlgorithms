d8499755188b3bb6da587a2ed19b5a415e6e0e3e
cezar <cezarantoniosouza@hotmail.com>
2021-01-25 23:59:24 -0300

    isPalindrome

--- LeetCode/Problems/IsPalindromeProblem.cs
+++ LeetCode/Problems/IsPalindromeProblem.cs
@@ -1,14 +1,31 @@
-
-
 using System;
 
 namespace ConsoleApp1.Problems
 {
   public static class IsPalindromeProblem
   {
+    // Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+    // Note: For the purpose of this problem, we define empty string as valid palindrome.
     public static bool implementation(string s)
     {
-      throw new Exception("Not implemented");
+      int left = 0, right = s.Length - 1;
+      do
+      {
+        if (!char.IsLetterOrDigit(s[left]))
+          left++;
+        else if (!char.IsLetterOrDigit(s[right]))
+          right--;
+        else
+        {
+          if (char.ToLower(s[left]) != char.ToLower(s[right]))
+            return false;
+
+          left++;
+          right--;
+        }
+      } while (left < right);
+
+      return true;       
     }
   }
 }
