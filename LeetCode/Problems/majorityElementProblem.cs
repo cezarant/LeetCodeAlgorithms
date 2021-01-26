286408b708d306e534cdd9933eb743747dcb045a
cezar <cezarantoniosouza@hotmail.com>
2021-01-25 23:36:54 -0300

    Majority element

--- LeetCode/Problems/majorityElementProblem.cs
+++ LeetCode/Problems/majorityElementProblem.cs
@@ -2,6 +2,9 @@ using System;
 
 public static class majorityElementProblem
 {
+  // Given an array nums of size n, return the majority element.
+  // The majority element is the element that appears more than ⌊n / 2⌋ times.
+  // You may assume that the majority element always exists in the array
   public static int implementation(int[] nums)
   {
     Array.Sort(nums);
