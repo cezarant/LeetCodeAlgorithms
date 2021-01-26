dc2a527ec71f6266a0df9b1b13e1654ac708926e
cezar <cezarantoniosouza@hotmail.com>
2021-01-26 00:11:54 -0300

    contains Duplicate

---
+++ LeetCode/Problems/containsDuplicateProblem.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1.Problems
+{
+  public static class containsDuplicateProblem
+  {
+    public static bool implementation(int[] nums){
+      int contDup = 0;
+      Array.Sort(nums);
+      for (int i = 0; i < nums.Length; i++)
+      {
+        if ((i + 1) < nums.Length)
+          if (nums[i] == nums[i + 1])
+              contDup++;
+      }
+      return contDup > 0; 
+    }
+  }
+}
---
+++ TestLeetCodeAlgorithms/UnitTests/containDuplicateUT.cs
@@ -0,0 +1,18 @@
+using ConsoleApp1.Problems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions; 
+
+namespace TestLeetCodeAlgorithms.UnitTests
+{  
+  [TestClass]
+  public class containDuplicateUT
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+      containsDuplicateProblem.implementation(new int[] { 1, 2, 3, 1 }).Should().Be(true);
+      containsDuplicateProblem.implementation(new int[] { 1, 2, 3, 4 }).Should().Be(false);
+      containsDuplicateProblem.implementation(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }).Should().Be(true);
+    }
+  }
+}
