91fd6f5670a980ac9a5ef4374355bc61cc55fdff
Cezar Antonio de Sousa <cezar.sousa@sysmap.com.br>
2021-02-09 22:40:09 -0300

    test

--- TestLeetCodeAlgorithms/UnitTests/containDuplicateUT.cs
+++ TestLeetCodeAlgorithms/UnitTests/containDuplicateUT.cs
@@ -9,9 +9,9 @@ public class containDuplicateUT
     [TestMethod]
     public void doIt()
     {
-        containsDuplicateProblem.implementation(new int[] { 1, 2, 3, 1 }).Should().Be(true);
-        containsDuplicateProblem.implementation(new int[] { 1, 2, 3, 4 }).Should().Be(false);
-        containsDuplicateProblem.implementation(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }).Should().Be(true);
+        //containsDuplicateProblem.implementation(new int[] { 4, 2, 3, 4 }).Should().Be(true);
+        //containsDuplicateProblem.implementation(new int[] { 1, 2, 3, 4 }).Should().Be(false);
+        containsDuplicateProblem.implementation(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12}).Should().Be(false);
     }
 }
 
--- TestLeetCodeAlgorithms/UnitTests/reverseStringUT.cs
+++ TestLeetCodeAlgorithms/UnitTests/reverseStringUT.cs
@@ -8,13 +8,16 @@ public class reverseStringUnitTest
     [TestMethod]
     public void doIt()
     {
+        string vs_path = System.IO.Directory.GetParent("..").ToString();        
         reverseStringProblem.implementation("cezar".ToCharArray()).Should().Equal("razec".ToCharArray());
+        
         reverseStringProblem.implementation("banana".ToCharArray()).Should().Equal("ananab".ToCharArray());
         reverseStringProblem.implementation("123456789123".ToCharArray()).Should().Equal("321987654321".ToCharArray());
 
-        string contentFile = System.IO.File.ReadAllText(string.Format(@"{0}\{1}", System.IO.Directory.GetParent(".."), @"TestCases\1.txt"));
-        string contentFileReverted = System.IO.File.ReadAllText(string.Format(@"{0}\{1}", System.IO.Directory.GetParent(".."), @"TestCases\1reverted.txt"));
-        reverseStringProblem.implementation(contentFile.ToCharArray()).Should().Equal(contentFileReverted.ToCharArray());
+        string contentFile = System.IO.File.ReadAllText(string.Format(@"{0}\{1}",vs_path, @"TestCases\1.txt"));
+        string contentFileReverted = System.IO.File.ReadAllText(string.Format(@"{0}\{1}",vs_path , @"TestCases\1reverted.txt"));
+        var conv = reverseStringProblem.implementation(contentFile.ToCharArray());          
+        conv.Should().Equal(contentFileReverted.ToCharArray());
         
         reverseStringProblem.implementation("".ToCharArray()).Should().Equal(string.Empty);
     }
